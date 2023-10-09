fddfb9a07f8ca11f12ec33071556a6dd2fde8b72
Rahaman2080 <rahaman2080@gmail.com>
2023-10-09 22:42:49 +0600

    added answering question file

--- src/Pages/EventCards/Feedback/Feedback.jsx
+++ src/Pages/EventCards/Feedback/Feedback.jsx
@@ -10,19 +10,19 @@ const Feedback = () => {
                             <div className="p-4">
                                 <p>Perfect decoration and perfect buffet for my littile sister birthday party</p>
                             </div>
-                            <figure className="avater"><img className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2" src="https://i.ibb.co/5Gvs1LQ/Rahman.jpg" alt="image" /></figure>
+                            <figure className="avater"><img className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2" src="https://i.ibb.co/jzk6yXy/person4.webp" alt="image" /></figure>
                         </div>
                         <div className="flex flex-col items-center justify-center text-center rounded-2xl w-60 bg-base-100 shadow-xl">
                             <div className="p-4">
                                 <p>Perfect decoration and perfect buffet for my littile sister birthday party</p>
                             </div>
-                            <figure className="avater"><img className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2" src="https://i.ibb.co/5Gvs1LQ/Rahman.jpg" alt="image" /></figure>
+                            <figure className="avater"><img className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2" src="https://i.ibb.co/KDNdmNL/person2.jpg" alt="image" /></figure>
                         </div>
                         <div className="flex flex-col items-center justify-center text-center rounded-2xl w-60 bg-base-100 shadow-xl">
                             <div className="p-4">
                                 <p>Perfect decoration and perfect buffet for my littile sister birthday party</p>
                             </div>
-                            <figure className="avater"><img className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2" src="https://i.ibb.co/5Gvs1LQ/Rahman.jpg" alt="image" /></figure>
+                            <figure className="avater"><img className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2" src="https://i.ibb.co/dP3BhJs/person1.jpg" alt="image" /></figure>
                         </div>
                         <div className="flex flex-col items-center justify-center text-center rounded-2xl w-60 bg-base-100 shadow-xl">
                             <div className="p-4">
@@ -38,19 +38,19 @@ const Feedback = () => {
                             <div className="p-4">
                                 <p>Perfect decoration and perfect buffet for my littile sister birthday party</p>
                             </div>
-                            <figure className="avater"><img className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2" src="https://i.ibb.co/5Gvs1LQ/Rahman.jpg" alt="image" /></figure>
+                            <figure className="avater"><img className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2" src="https://i.ibb.co/jzk6yXy/person4.webp" alt="image" /></figure>
                         </div>
                         <div className="flex flex-col items-center justify-center text-center rounded-2xl w-60 bg-base-100 shadow-xl">
                             <div className="p-4">
                                 <p>Perfect decoration and perfect buffet for my littile sister birthday party</p>
                             </div>
-                            <figure className="avater"><img className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2" src="https://i.ibb.co/5Gvs1LQ/Rahman.jpg" alt="image" /></figure>
+                            <figure className="avater"><img className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2" src="https://i.ibb.co/KDNdmNL/person2.jpg" alt="image" /></figure>
                         </div>
                         <div className="flex flex-col items-center justify-center text-center rounded-2xl w-60 bg-base-100 shadow-xl">
                             <div className="p-4">
                                 <p>Perfect decoration and perfect buffet for my littile sister birthday party</p>
                             </div>
-                            <figure className="avater"><img className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2" src="https://i.ibb.co/5Gvs1LQ/Rahman.jpg" alt="image" /></figure>
+                            <figure className="avater"><img className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2" src="https://i.ibb.co/dP3BhJs/person1.jpg" alt="image" /></figure>
                         </div>
                         <div className="flex flex-col items-center justify-center text-center rounded-2xl w-60 bg-base-100 shadow-xl">
                             <div className="p-4">
@@ -60,6 +60,8 @@ const Feedback = () => {
                         </div>
                     </div>
                 </div>
+                
+                
             </div>
             <div className="flex justify-center w-full py-2 gap-2">
                 <a href="#item1" className="btn btn-xs">1</a>
--- src/Routers/About/About.jsx
+++ src/Routers/About/About.jsx
@@ -8,71 +8,35 @@ const About = () => {
             <div style={{ fontFamily: "Time new Romans" }} className="max-w-5xl mx-auto text-center py-10">
                 <h1 className="text-4xl font-bold">Hello! We are an Event Planning Agency</h1>
                 <p className="py-8 text-purple-700 font-semibold text-xl">As the premier event planning company in Chittagong, we know that it’s not “one size fits all”. Each event and client is unique and we believe our services should be as well. We know that it should be “Can I hire a planner?” not “Can I afford one?”.</p>
-                <div>
+                <div className="my-5">
                     <h1 className="text-4xl font-bold text-center">Our Staff</h1>
                 </div>
-                <div className="carousel w-full my-8">
-                <div id="item1" className="carousel-item w-full">
-                    <div className="grid grid-cols-2 md:grid-cols-4 gap-10">
-                        <div className="flex flex-col items-center justify-center text-center rounded-2xl w-60 bg-base-100 shadow-xl">
-                            <div className="p-4">
-                                <p>Perfect decoration and perfect buffet for my littile sister birthday party</p>
-                            </div>
-                            <figure className="avater"><img className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2" src="https://i.ibb.co/5Gvs1LQ/Rahman.jpg" alt="image" /></figure>
+                <div className="grid grid-cols-2 md:grid-cols-4 gap-10">
+                    <div className="flex flex-col items-center justify-center text-center rounded-2xl w-60 h-60 bg-base-100 shadow-xl">
+                        <div className="p-4">
+                            <p className="text-purple-700 font-bold text-2xl">Abdur Rahman</p>
                         </div>
-                        <div className="flex flex-col items-center justify-center text-center rounded-2xl w-60 bg-base-100 shadow-xl">
-                            <div className="p-4">
-                                <p>Perfect decoration and perfect buffet for my littile sister birthday party</p>
-                            </div>
-                            <figure className="avater"><img className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2" src="https://i.ibb.co/5Gvs1LQ/Rahman.jpg" alt="image" /></figure>
+                        <figure className="avater"><img className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2" src="https://i.ibb.co/5Gvs1LQ/Rahman.jpg" alt="image" /></figure>
+                    </div>
+                    <div className="flex flex-col items-center justify-center text-center rounded-2xl w-60 bg-base-100 shadow-xl">
+                        <div className="p-4">
+                        <p className="text-purple-700 font-bold text-2xl">Moniruzzaman</p>
                         </div>
-                        <div className="flex flex-col items-center justify-center text-center rounded-2xl w-60 bg-base-100 shadow-xl">
-                            <div className="p-4">
-                                <p>Perfect decoration and perfect buffet for my littile sister birthday party</p>
-                            </div>
-                            <figure className="avater"><img className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2" src="https://i.ibb.co/5Gvs1LQ/Rahman.jpg" alt="image" /></figure>
+                        <figure className="avater"><img className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2" src="https://i.ibb.co/dP3BhJs/person1.jpg" alt="image" /></figure>
+                    </div>
+                    <div className="flex flex-col items-center justify-center text-center rounded-2xl w-60 bg-base-100 shadow-xl">
+                        <div className="p-4">
+                        <p className="text-purple-700 font-bold text-2xl">Morshed Emon</p>
                         </div>
-                        <div className="flex flex-col items-center justify-center text-center rounded-2xl w-60 bg-base-100 shadow-xl">
-                            <div className="p-4">
-                                <p>Perfect decoration and perfect buffet for my littile sister birthday party</p>
-                            </div>
-                            <figure className="avater"><img className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2" src="https://i.ibb.co/5Gvs1LQ/Rahman.jpg" alt="image" /></figure>
+                        <figure className="avater"><img className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2" src="https://i.ibb.co/KDNdmNL/person2.jpg" alt="image" /></figure>
+                    </div>
+                    <div className="flex flex-col items-center justify-center text-center rounded-2xl w-60 bg-base-100 shadow-xl">
+                        <div className="p-4">
+                        <p className="text-purple-700 font-bold text-2xl">Aliza Alex</p>
                         </div>
+                        <figure className="avater"><img className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2" src="https://i.ibb.co/jzk6yXy/person4.webp" alt="image" /></figure>
                     </div>
                 </div>
-                <div id="item2" className="carousel-item w-full">
-                    <div className="grid grid-cols-2 md:grid-cols-4 gap-10">
-                        <div className="flex flex-col items-center justify-center text-center rounded-2xl w-60 bg-base-100 shadow-xl">
-                            <div className="p-4">
-                                <p>Perfect decoration and perfect buffet for my littile sister birthday party</p>
-                            </div>
-                            <figure className="avater"><img className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2" src="https://i.ibb.co/5Gvs1LQ/Rahman.jpg" alt="image" /></figure>
-                        </div>
-                        <div className="flex flex-col items-center justify-center text-center rounded-2xl w-60 bg-base-100 shadow-xl">
-                            <div className="p-4">
-                                <p>Perfect decoration and perfect buffet for my littile sister birthday party</p>
-                            </div>
-                            <figure className="avater"><img className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2" src="https://i.ibb.co/5Gvs1LQ/Rahman.jpg" alt="image" /></figure>
-                        </div>
-                        <div className="flex flex-col items-center justify-center text-center rounded-2xl w-60 bg-base-100 shadow-xl">
-                            <div className="p-4">
-                                <p>Perfect decoration and perfect buffet for my littile sister birthday party</p>
-                            </div>
-                            <figure className="avater"><img className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2" src="https://i.ibb.co/5Gvs1LQ/Rahman.jpg" alt="image" /></figure>
-                        </div>
-                        <div className="flex flex-col items-center justify-center text-center rounded-2xl w-60 bg-base-100 shadow-xl">
-                            <div className="p-4">
-                                <p>Perfect decoration and perfect buffet for my littile sister birthday party</p>
-                            </div>
-                            <figure className="avater"><img className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2" src="https://i.ibb.co/5Gvs1LQ/Rahman.jpg" alt="image" /></figure>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="flex justify-center w-full py-2 gap-2">
-                <a href="#item1" className="btn btn-xs">1</a>
-                <a href="#item2" className="btn btn-xs">2</a>
-            </div>
             </div>
 
         </div>
